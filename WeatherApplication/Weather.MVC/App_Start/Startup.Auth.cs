962b48644fa2f77b2b2a33c83fdefbc5b1456c96
vl222cu <vl222cu@student.lnu.se>
2015-02-25 20:16:34 +0100

    Fix

--- WeatherApplication/Weather.MVC/App_Start/Startup.Auth.cs
+++ WeatherApplication/Weather.MVC/App_Start/Startup.Auth.cs
@@ -37,8 +37,8 @@ namespace Weather.MVC
             //   appSecret: "");
 
             app.UseGoogleAuthentication(
-                clientId: "327867939996-m4qt9dkrhf75oc1nf9pr9fep7piq6n0k.apps.googleusercontent.com",
-                clientSecret: "nZSLTxuY36vJ8RfMoqnA-sgb");
+                clientId: "xxx",
+                clientSecret: "xxx");
         }
     }
 }
