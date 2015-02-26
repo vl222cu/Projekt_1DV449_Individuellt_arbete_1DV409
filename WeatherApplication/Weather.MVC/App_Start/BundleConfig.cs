a0a26998297ca91870300163d122938e6adc1faf
vl222cu <vl222cu@student.lnu.se>
2015-02-26 21:15:06 +0100

    Lagt till dialogruta vid offlineläge

--- WeatherApplication/Weather.MVC/App_Start/BundleConfig.cs
+++ WeatherApplication/Weather.MVC/App_Start/BundleConfig.cs
@@ -9,10 +9,14 @@ namespace Weather.MVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-*",
+                        "~/Scripts/jquery-ui-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/customjs").Include(
+                       "~/Scripts/MyCustomJS/StatusCheck.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
